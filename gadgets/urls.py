be03ab15933ad2449372d8d9284bf551805b99dc
Mahabubul Hasan <mahabubul470@gmail.com>
2023-06-02 01:48:07 +0600

    add: product redirect view

--- gadgets/urls.py
+++ gadgets/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for gadgets project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.views import View
+
+
+class RedirectProductView(View):
+
+    def get(self, request):
+        return redirect('products/list')
+
 
 urlpatterns = [
+    path("", RedirectProductView.as_view(), name="home"),
     path('admin/', admin.site.urls),
-]
+    path('products/', include('product.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
